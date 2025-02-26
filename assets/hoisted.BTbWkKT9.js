2087bd932301d6fedab2d34a99e868396e858ac1
Fábio Novais <fabio.novais@gmail.com>
2025-02-25 23:38:25 -0300

    Updates

--- assets/hoisted.BTbWkKT9.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.CtjKOE4_.js";import"./Depoimentos.astro_astro_type_script_index_0_lang.DuhcXgtZ.js";import"./hoisted.B9qBVkyD.js";import"./swiper-core.DdILsi2M.js";import"./pagination.2Dv2G4Uq.js";
---
+++ assets/hoisted.CpNM_ogQ.js
@@ -0,0 +1,1 @@
+import"./Depoimentos.astro_astro_type_script_index_0_lang.DuhcXgtZ.js";import"./hoisted.CtjKOE4_.js";import"./hoisted.B9qBVkyD.js";import"./swiper-core.DdILsi2M.js";import"./pagination.2Dv2G4Uq.js";
